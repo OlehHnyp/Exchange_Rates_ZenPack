ae651f90a8522ad65b1020fffba41435b98515eb
OlehHnyp <lefk6230@gmail.com>
2021-11-30 12:19:26 +0200

    Agent behaviour changed.

--- ZenPacks.training.ER/ZenPacks/training/ER/agent.py
+++ ZenPacks.training.ER/ZenPacks/training/ER/agent.py
@@ -1,7 +1,7 @@
 from twisted.web.client import Agent
 from twisted.internet import reactor
 from twisted.internet.protocol import Protocol
-from twisted.internet.defer import Deferred
+from twisted.internet.defer import Deferred, inlineCallbacks, returnValue
 
 
 class NotExpectedResponseCodeError(Exception):
@@ -35,22 +35,18 @@ class CustomHttpAgent:
         self.reactor = reactor
         self.agent = Agent(self.reactor)
 
+    @inlineCallbacks
     def get_response_body(self, *args, **kwargs):
-        response = self.agent.request("GET", *args, **kwargs)
-        response.addCallback(self._check_status_code)
-        response.addCallback(self._get_raw_body_data)
-        return response
+        response = yield self.agent.request("GET", *args, **kwargs)
+        response_body = yield self._get_raw_body_data(response)
+        if response.code != 200:
+            raise NotExpectedResponseCodeError(
+                "Not Expected response code: {}, {} {}".format(response.code, response.phrase, response_body)
+            )
+        returnValue(response_body)
 
     @staticmethod
     def _get_raw_body_data(response):
         finished = Deferred()
         response.deliverBody(_BodyAgent(finished, response.code))
         return finished
-
-    @staticmethod
-    def _check_status_code(response):
-        if response.code != 200:
-            raise NotExpectedResponseCodeError(
-                "Not Expected response code: {}, {}".format(response.code, response.phrase)
-            )
-        return response
